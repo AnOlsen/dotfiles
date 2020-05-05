2bcc17d1514be988f5cc0503b1933ffd3be0635e
André Olsen <andreolsen4200@gmail.com>
2020-05-05 23:22:19 +0200

    Update stuff

--- apps/install.sh
+++ apps/install.sh
@@ -26,6 +26,10 @@ do
   sudo snap install $app
 done < "$applications"
 
+# Install Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Install composer via PHP
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
---
+++ home/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Setting up home"
+
+git clone git@github.com:emaerket/powershell-scripts.git $SCRIPTS/powershell
+git clone git@github.com:emaerket/emaerket.git $HOME/projects/emaerket
+
+echo "Home setup complete"
